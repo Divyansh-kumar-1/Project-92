6c80725b2ffb260ae87a161d7d33591a422b4b4a
Divyansh-kumar-1 <81742026+Divyansh-kumar-1@users.noreply.github.com>
2022-06-23 14:56:49 +0530

    Add files via upload

---
+++ quiz_game.js
@@ -0,0 +1,40 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_name").innerHTML = player1_name;
+document.getElementById("player2_name").innerHTML = player2_name;
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+function send(){
+    number_1 = document.getElementById("Number_1").value;
+    number_2 = document.getElementById("Number_2").value;
+
+    actual_answer = parseInt(number_1) * parseInt(number_2);
+
+    question_number = "<h4>" + number_1 + " X " + number_2 + "</h4>";
+
+    input_box = "<br> Answer : <input type='text' id='input_check_box'> ";
+
+    check_button ="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+
+    row = question_number + input_box + check_button;
+
+    document.getElementById("output").innerHTML = row;
+
+    document.getElementById("Number_1").value = " ";
+    document.getElementById("Number_2").value = " ";
+}
+var Answer_turn = "player1";
+var question_turn = "player2";
+function check(){
+ get_answer = document.getElementById("input_check_box").value; 
+ if(get_answer == actual_answer){
+    update_player_score = player1_score + 1;
+    document.getElementById("player1_score").innerHTML = update_player_score;
+ }
+ else{
+    update_player_score = player2_score + 1;
+    document.getElementById("player2_score").innerHTML = update_player_score;
+ }
+}
